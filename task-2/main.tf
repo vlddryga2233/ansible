54f505e63b96fdee5788f9de5d8d2853b32295e6
vlddryga2233 <vlddryga@gmail.com>
2021-09-28 15:51:10 +0300

    edit tree

---
+++ task-2/main.tf
@@ -0,0 +1,140 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.83.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+  credentials = var.credentials
+}
+
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+data "google_compute_image" "linux" {
+  family  = "ubuntu-2004-lts"
+  project = "ubuntu-os-cloud"
+}
+
+data "google_compute_image" "debian" {
+  family  = "debian-10"
+  project = "debian-cloud"
+}
+
+resource "google_compute_instance" "default1" {
+  name         = "nginx-webserver-1"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  tags = ["http-server"]
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.linux.self_link
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Hello world!'"]
+    connection {
+      type        = "ssh"
+      host        = self.network_interface.0.access_config.0.nat_ip
+      user        = "vlad"
+      private_key = file(var.ssh_key_private)
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i '${self.network_interface.0.access_config.0.nat_ip},' nginxweb.yml"
+  }
+  service_account {
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+resource "google_compute_instance" "default2" {
+  name         = "nginx-webserver-2"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  tags = ["http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.debian.self_link
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+  provisioner "remote-exec" {
+    inline = ["echo 'Hello world!'"]
+    connection {
+      type        = "ssh"
+      host        = self.network_interface.0.access_config.0.nat_ip
+      user        = "vlad"
+      private_key = file(var.ssh_key_private)
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${self.network_interface.0.access_config.0.nat_ip},' nginxweb.yml"
+  }
+  service_account {
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_instance" "default3" {
+  name         = "nginx-load-balancer"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  tags = ["http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.linux.self_link
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+  provisioner "remote-exec" {
+    inline = ["echo 'Hello world!'"]
+    connection {
+      type        = "ssh"
+      host        = self.network_interface.0.access_config.0.nat_ip
+      user        = "vlad"
+      private_key = file(var.ssh_key_private)
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${self.network_interface.0.access_config.0.nat_ip},' -e 'server1=${resource.google_compute_instance.default1.network_interface.0.access_config.0.nat_ip} server2=${resource.google_compute_instance.default2.network_interface.0.access_config.0.nat_ip}'  lb.yml"
+  }
+  service_account {
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
--- terraform.tfvars
+++
@@ -1,9 +0,0 @@
-region = "us-central1"
-
-zone = "us-central1-a"
-
-credentials = "./app_cred.json"
-
-project = "sage-outrider-322609"
-
-ssh_key_private = "/home/vlad/.ssh/id_rsa"
